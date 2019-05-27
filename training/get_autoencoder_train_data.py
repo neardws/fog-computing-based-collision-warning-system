ce0cd9ed4c4bc2addb2922a965e679303598fb08
neardw <singleangel@vip.qq.com>
2019-05-27 20:59:09 +0800

    add get_autoencoder_train

--- training/get_autoencoder_train_data.py
+++ training/get_autoencoder_train_data.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+import os
+import random
 import multiprocessing as mp
 TRAIN_DATA = r'E:\NearXu\train_data\train_'
 AUTOENCODER_TRAIN_PATH_CSV = r'E:\NearXu\autoencoder2\train_'
@@ -12,32 +15,86 @@ global test_csv_file
 test_csv_file = open(AUTOENCODER_TEST_CSV, 'a+', encoding='utf-8')
 
 
+def merge_file():
+    Folder_Path = r'E:\NearXu\autoencoder2'  # 要拼接的文件夹及其完整路径，注意不要包含中文
+    SaveFile_Path = r'E:\NearXu\autoencoder2'  # 拼接后要保存的文件路径
+    SaveFile_Name = r'all.csv'  # 合并后要保存的文件名
+
+    # 修改当前工作目录
+    os.chdir(Folder_Path)
+    # 将该文件夹下的所有文件名存入一个列表
+    file_list = os.listdir()
+
+    # 读取第一个CSV文件并包含表头
+    df = pd.read_csv(Folder_Path + '\\' + file_list[0])  # 编码默认UTF-8，若乱码自行更改
+
+    # 将读取的第一个CSV文件写入合并后的文件保存
+    df.to_csv(SaveFile_Path + '\\' + SaveFile_Name, encoding="utf_8_sig", index=False)
+
+    # 循环遍历列表中各个CSV文件名，并追加到合并后的文件
+    for i in range(1, len(file_list) - 1):
+        df = pd.read_csv(Folder_Path + '\\' + file_list[i])
+        df.to_csv(SaveFile_Path + '\\' + SaveFile_Name, encoding="utf_8_sig", index=False, header=False, mode='a+')
+
+
 def process(trace_id):
     #print(trace_id)
     with open(AUTOENCODER_TRAIN_PATH_CSV + str(int(trace_id)) + '.csv', 'a+', encoding='utf-8') as csv_file:
         with open(TRAIN_DATA + str(int(trace_id)) + '.txt', 'r+', encoding='utf-8') as file:
             for line in file:
                 points = line.split(' ')
-                print(len(points))
-                random_number = np.random.randint(len(points))
+                # print(len(points))
+                # random_number = np.random.randint(len(points))
                 # print(random_number)
                 # print(points[random_number])
-                xy = points[random_number].split(',')
-                # print(xy)
-                # print(len(xy))
-                if (len(xy) == 2):
-                    x = xy[0]
-                    y = xy[1]
-                    try:
-                        float(x)
-                        float(y)
-                        csv_str = str(x) + ',' + str(y) + '\n'
-                        if csv_str.count(',') > 1:
-                            print('count > 1')
-                        else:
-                            train_csv_file.writelines(csv_str)
-                    except ValueError:
-                        print("ValueError")
+                for point in points:
+                    xy = point.split(',')
+                    # print(xy)
+                    # print(len(xy))
+                    if (len(xy) == 2):
+                        x = xy[0]
+                        y = xy[1]
+                        try:
+                            float(x)
+                            float(y)
+                            csv_str = str(x) + ',' + str(y) + '\n'
+                            if csv_str.count(',') > 1:
+                                print('count > 1')
+                            else:
+                                csv_file.writelines(csv_str)
+                        except ValueError:
+                            print("ValueError")
+                # xy = points[random_number].split(',')
+                # # print(xy)
+                # # print(len(xy))
+                # if (len(xy) == 2):
+                #     x = xy[0]
+                #     y = xy[1]
+                #     try:
+                #         float(x)
+                #         float(y)
+                #         csv_str = str(x) + ',' + str(y) + '\n'
+                #         if csv_str.count(',') > 1:
+                #             print('count > 1')
+                #         else:
+                #             csv_file.writelines(csv_str)
+                #     except ValueError:
+                #         print("ValueError")
+
+
+def process_random(trace_id):
+    with open(AUTOENCODER_TRAIN_PATH_CSV + str(int(trace_id)) + '.csv', 'r+', encoding='utf-8') as csv_file:
+        print(trace_id)
+        lines = csv_file.readlines()
+        lines_number = len(lines)
+        print(lines_number)
+        random_list = range(0, lines_number)
+        random_train_id = random.sample(random_list, int(0.5 * lines_number))
+        random_test_id = random.sample(random_list, int(0.1 * lines_number))
+        for id in random_train_id:
+            train_csv_file.writelines(lines[id])
+        for id in random_test_id:
+            test_csv_file.writelines(lines[id])
 
 
 def main():
@@ -46,11 +103,12 @@ def main():
     for i in range(29):
         trace_id = []
         trace_id.append(i)
-        jobs.append(pool.apply_async(process, trace_id))
+        jobs.append(pool.apply_async(process_random, trace_id))
     for job in jobs:
         job.get()
     pool.close()
 
+    # merge_file()
 
 
 if __name__ == '__main__':
--- training/taining_by_autoencoder.py
+++ training/taining_by_autoencoder.py
@@ -7,15 +7,21 @@ from keras.layers import Dense,Input
 AUTOENCODER_TRAIN_CSV = r'E:\NearXu\autoencoder2\train.csv'
 AUTOENCODER_TEST_CSV = r'E:\NearXu\autoencoder2\test.csv'
 
+AUTOENCODER_MODEL = r'E:\NearXu\autoencoder2\autoencoder2.h5'
+ENCODER_MODEL = r'E:\NearXu\autoencoder2\encoder2.h5'
+DECODER_MODEL = r'E:\NearXu\autoencoder2\decoder2.h5'
+
 
 def main():
     # 训练数据集
     df_train = pd.read_csv(AUTOENCODER_TRAIN_CSV)
     train = np.array(df_train)
+    train = train.astype('float32') / 30.
     print(train.shape)
     # 测试数据集
     df_test = pd.read_csv(AUTOENCODER_TEST_CSV)
     test = np.array(df_test)
+    test = test.astype('float32') / 30.
     print(test.shape)
 
     #压缩特征到1维
@@ -24,28 +30,41 @@ def main():
     input_two_dim = Input(shape=(2,))
 
     # 编码层
-    encoded = Dense(64, activation='relu')(input_two_dim)
-    encoded = Dense(10, activation='relu')(encoded)
-    encoder_output = Dense(encoding_dim)(encoded)
+    encoded = Dense(encoding_dim * 10, activation='relu')(input_two_dim)
+    encoded = Dense(encoding_dim * 64, activation='relu')(encoded)
+    encoded = Dense(encoding_dim * 10, activation='relu')(encoded)
+    encoded = Dense(encoding_dim)(encoded)
 
     # 解码层
-    decoded = Dense(10, activation='tanh')(encoder_output)
-    decoded = Dense(64, activation='relu')(decoded)
+    decoded = Dense(encoding_dim * 10, activation='relu')(encoded)
+    decoded = Dense(encoding_dim * 64, activation='relu')(decoded)
+    decoded = Dense(encoding_dim * 10, activation='relu')(decoded)
     decoded = Dense(2, activation='tanh')(decoded)
 
     # 构建自编码器
     autoencoder = Model(inputs= input_two_dim, outputs= decoded)
 
-    # # 构建编码模型
-    # encoder = Model(inputs= input_two_dim, outputs= encoded_output)
-    #
-    # # 构建解码模型
-    # decoder = Model(inputs= encoded_output, outputs= decoded)
+    # 构建编码模型
+    encoder = Model(inputs= input_two_dim, outputs= encoded)
+
+    # 构建解码模型
+    encoder_input = Input(shape=(encoding_dim,))
+
+    decoded_layer1 = autoencoder.layers[-4]
+    decoded_layer2 = autoencoder.layers[-3]
+    decoded_layer3 = autoencoder.layers[-2]
+    decoded_layer4 = autoencoder.layers[-1]
+
+    decoder = Model(inputs= encoder_input, outputs= decoded_layer4(decoded_layer3(decoded_layer2(decoded_layer1(encoder_input)))))
 
     # 编译模型
     autoencoder.compile(optimizer='adam', loss='mse')
 
-    autoencoder.fit(train, train, epochs=200, batch_size=256, shuffle=True, validation_data=(test, test))
+    autoencoder.fit(train, train, epochs=10, batch_size=128, shuffle=True, validation_data=(test, test))
+
+    autoencoder.save(AUTOENCODER_MODEL)
+    encoder.save(ENCODER_MODEL)
+    decoder.save(DECODER_MODEL)
 
 
 
