da53ce8a24bd1d387dfac6a1e76627bf65280709
neardw <singleangel@vip.qq.com>
2019-07-11 20:44:59 +0800

    update get test data

---
+++ experiment/get_test_data.py
@@ -0,0 +1,110 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize
+
+
+def f_1(x, A, B):
+    return A * x +  B
+
+
+def f_2(x, A, B, C):
+    return A * x * x + B * x + C
+
+
+def curve_fitting(map_x, range, trace_x, trace_y):
+    plt.figure()
+    plt.scatter(trace_x[:], trace_y[:], 10, 'red')
+
+    A, B, C = optimize.curve_fit(f_2, trace_x, trace_y)[0]
+    x = np.arange(map_x-range, map_x+range, 0.01)
+    y = A * x * x + B * x + C
+    plt.plot(x, y, 'green')
+
+    plt.show()
+
+
+def get_trace(time, scenario, range, during):
+    pass
+
+
+def get_csv_file(time):
+    csv_file = r'E:\NearXu\trace\trace_'
+    dic_csv_file = {
+        '1am'   :   csv_file + '0.csv',
+        '2am'   :   csv_file + '0.csv',
+        '3am'   :   csv_file + '0.csv',
+        '4am'   :   csv_file + '0.csv',
+        '5am'   :   csv_file + '0.csv',
+        '6am'   :   csv_file + '0.csv',
+        '7am'   :   csv_file + '3.csv',
+        '8am'   :   csv_file + '7.csv',
+        '9am'   :   csv_file + '8.csv',
+        '10am'  :   csv_file + '9.csv',
+        '11am'  :   csv_file + '10.csv',
+        '12am'  :   csv_file + '12.csv',
+        '1pm'   :   csv_file + '13.csv',
+        '2pm'   :   csv_file + '14.csv',
+        '3pm'   :   csv_file + '15.csv',    # no good
+        '4pm'   :   csv_file + '17.csv',
+        '5pm'   :   csv_file + '20.csv',
+        '6pm'   :   csv_file + '23.csv',
+        '7pm'   :   csv_file + '24.csv',    # no good
+        '8pm'   :   csv_file + '26.csv',
+        '9pm'   :   csv_file + '27.csv',
+        '10pm'  :   csv_file + '27.csv',
+        '11pm'  :   csv_file + '28.csv'
+    }
+    try:
+        return dic_csv_file[time]
+    except KeyError:
+        print("Key Error in get_csv_file")
+
+
+def get_start_time(time):
+    dic_start_time = {
+        '1am'   :   3600,
+        '2am'   :   7200,
+        '3am'   :   10800,
+        '4am'   :   14400,
+        '5am'   :   18000,
+        '6am'   :   21600,
+        '7am'   :   25200,
+        '8am'   :   28800,
+        '9am'   :   32400,
+        '10am'  :   36000,
+        '11am'  :   39600,
+        '12am'  :   43200,
+        '1pm'   :   46800,
+        '2pm'   :   50400,
+        '3pm'   :   54000,
+        '4pm'   :   57600,
+        '5pm'   :   61200,
+        '6pm'   :   64800,
+        '7pm'   :   68400,
+        '8pm'   :   72000,
+        '9pm'   :   75600,
+        '10pm'  :   79200,
+        '11pm'  :   82800
+    }
+    try:
+        return dic_start_time[time]
+    except KeyError:
+        print("Key Error in get_start_time")
+
+
+def get_scenario_xy(number):
+    dic_scenario_xy = {
+        '1' :  [5241.17,14185.2],
+        '2' :  [6097.06,14870.0],
+        '3' :  [6581.00,26107.6],
+        '4' :  [7653.15,19486.6],
+        '5' :  [9447.04,18721.4],
+        '6' :  [10422.00,12465.3],
+        '7' :  [10435.80,17528.2],
+        '8' :  [11227.50,20388.4],
+        '9' :  [11550.60,18791.4]
+    }
+    try:
+        return dic_scenario_xy[number]
+    except KeyError:
+        print("Key Error in get_scenario_xy")
