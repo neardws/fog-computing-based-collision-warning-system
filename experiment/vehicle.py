c5947d2a85c4d924998729e3855b09ee462a913c
neardw <singleangel@vip.qq.com>
2019-07-18 10:18:53 +0800

    finish vehicle

--- experiment/vehicle.py
+++ experiment/vehicle.py
@@ -1,17 +1,29 @@
+from transmission_model import transmission_model
 class vehicle:
-    vehicleID = 0
-    location_x = 0
-    location_y = 0
-    transmission_delay = 0
-    packet_loss = False
+    def __init__(self, packet_loss_rate):
+        self.vehicleID = None
+        self.location_x = None
+        self.location_y = None
+        self.transmission_delay = None
+        self.packet_loss = None
+        self.trans_model = transmission_model(packet_loss_rate)
 
-    def __init__(self):
-        pass
+    def set_transmission_delay(self):
+        self.transmission_delay = self.trans_model.get_transmission_delay()
 
-    def __setattr__(self, key, value):
-        if key == 'vehicleID':
-            self.vehicleID = value
-        elif key == 'location_x':
-            self.location_x = value
+    def set_packet_loss(self):
+        self.packet_loss = self.trans_model.get_packet_loss()
 
+    def set_vehicleID(self, id):
+        self.vehicleID = id
 
+    def set_location(self, x, y):
+        self.location_x = x
+        self.location_y = y
+
+if __name__ == '__main__':
+    v = vehicle(3.08)
+    v.set_packet_loss()
+    v.set_transmission_delay()
+    print(v.packet_loss)
+    print(v.transmission_delay)
