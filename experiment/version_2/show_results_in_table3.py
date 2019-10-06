98204653cd53cf87bdbd9314a5d176cea7f3b0ad
Near D.WS <near@NeardeMacBook-Pro.local>
2019-10-06 12:54:14 +0800

    different headway

---
+++ experiment/version_2/show_results_in_table3.py
@@ -0,0 +1,54 @@
+from prettytable import PrettyTable
+import json
+import ast
+
+File_Head = '/Users/near/NearXu/result/different_headway/dr_result'
+
+
+def get_results(type, start_time, scenario, packet_loss_rate, headway):
+    result_name = File_Head + '_type_' + str(type) + '_time_' + str(start_time) + '_scenario_' + str(scenario) + '_plr_' \
+                  + str(packet_loss_rate) + '_headway_' + str(headway) + '.txt'
+    with open(result_name, 'r+') as file:
+        result = file.readline()
+        result = result.replace("'", '"')
+        result_json = json.loads(result)
+        return result_json
+
+
+def show_information():
+    types = ['result_threading_different_scenario_cloud', 'result_threading_different_scenario_fog',
+             'result_threading_different_scenario_fog_re', 'result_threading_different_scenario_fog_gvcw']
+
+    default_packet_loss_rate = 3
+    default_headway = 3
+
+    default_start_time = '7pm'
+    default_scenario = '6'
+    different_headway = [1, 2, 3, 4, 5]
+
+    table = PrettyTable(['time', 'scenario', 'headway', 'type', 'precision', 'recall'])
+
+    for headway in different_headway:
+        for type in types:
+            time = default_start_time
+            scenario = default_scenario
+            result_json = get_results(type, time, scenario, default_packet_loss_rate, headway)
+            precision = result_json['precision']
+            recall = result_json['recall']
+            table_row = []
+            table_row.append(time)
+            table_row.append(scenario)
+            table_row.append(headway)
+            table_row.append(type)
+            table_row.append(precision)
+            table_row.append(recall)
+            table.add_row(table_row)
+    print(table)
+
+
+if __name__ == '__main__':
+    # default_packet_loss_rate = 3
+    # default_headway = 3
+    # get_results(type='result_threading_different_scenario_fog_gvcw', start_time='7am', scenario='2', packet_loss_rate=default_packet_loss_rate, headway=default_headway)
+
+    show_information()
